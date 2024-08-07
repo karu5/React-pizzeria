f37c772f3a9f34bf50e497962994dab662ba49e6
karu5 <fabiolakmschmidt@gmail.com>
2024-08-07 13:09:42 -0400

    Primer trabajo de react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './componentes/Navbar'
+import Home from './componentes/Home'
+import Footer from './componentes/Footer'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
