93e0046ef9054d3f87b6a2267d221b2fa115ae3f
karu5 <fabiolakmschmidt@gmail.com>
2024-08-12 17:31:25 -0400

    Formulario Pizzaria

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import Navbar from './componentes/Navbar'
 import Home from './componentes/Home'
 import Footer from './componentes/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import RegisterPage from './componentes/RegisterPage'
+import LoginPage from './componentes/LoginPage'
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      {/*<Home />
+      <LoginPage/>*/}
+      <RegisterPage/>
+      {/*<Home />
+      <Home/>*/}     
       <Footer />
     </>
   )
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -4,7 +4,7 @@ import './styles/Footer.css'
 const Footer = () => {
   return (
     <footer>
-      <p>© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+      <p>© 2024 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
     </footer>
   )
 }
